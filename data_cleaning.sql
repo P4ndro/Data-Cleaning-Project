8ad9d82c11762872c295f453c41fcc44098896d4
Sandro Iobidze <sandro02050502@gmail.com>
2025-05-04 01:19:21 +0400

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,7 +1,1 @@
-//data-cleaning-layoffs-sql
-├── 01_remove_duplicates.sql
-├── 02_create_staging2_and_dedupe.sql
-├── 03_standardize_fields.sql
-├── 04_handle_nulls_and_backfill.sql
-├── 05_finalize_cleanup.sql
-└── README.md
+
